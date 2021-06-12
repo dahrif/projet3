41aaa99b0c57e553d393db8669811ef6c6f13ae3
dahri fatima <dahri.fatima.dev@gmail.com>
2021-06-12 21:09:39 +0100

    save

--- certificat.js
+++ certificat.js
@@ -4,7 +4,7 @@ window.addEventListener('load', () => {
     var prenom = JSON.parse(localStorage.getItem('PRENOM'));
 
     for(let i=0 ; i< nom.length ; i++ )
-    {document.getElementById('certif').innerHTML +="<body> <div class='container'><div class='logo'>Solicode<div><div class='marquee'>Certificate of Completion</div><div class='assignment'>This certificate is presented to</div><div class='person'>"+ nom[i] + '&nbsp;' + prenom[i] + "</div><div class='reason'>For deftly defying the laws of gravity<br/>and flying high</div></div></body>"+"<br>";
+    {document.getElementById('certif').innerHTML +="<body><div class='container'><div class='logo'>Solicode</div><div class='marquee'>Certificate of Completion</div><div class='assignment'>This certificate is presented to</div><div class='person'>"+nom +prenom +"</div><div class='reason'>For deftly defying the laws of gravity<br/>and flying high</div><div><img src='Stamp.svg' class='stamp'></div></div></body>"+"<br>";
     
 
 }
